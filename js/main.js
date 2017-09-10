5aa32cea5dde657c265e763157ed6e366ef6e0c8
gloit <gloit042@gmail.com>
2017-09-10 22:56:23 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+word = 'Do not multiply entities beyond necessity'
+by = '-- William of Occam'
+
 new Vue({
   el: '#app',
   data: function () {
     return {
       message: 'Hello',
-      myName: '李嘉豪'
+      myName: '李嘉豪',
+      word: word,
+      by: by
     }
   }
 })
